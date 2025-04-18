6e4c4e5aad1b32bfff632463d954fc4d1f2556c7
floklein <4128942+floklein@users.noreply.github.com>
2025-04-18 06:38:31 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import { ModeToggle } from "@/components/mode-toggle";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import type { Metadata } from "next";
+import Link from "next/link";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -24,12 +26,26 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <header className="flex items-center gap-4 sm:gap-8 border-b h-12 sm:h-16 flex-shrink-0 px-4 sm:px-8">
-            <h1 className="text-2xl font-serif">Artguessr</h1>
+            <h1 className="text-2xl font-serif">
+              Artguessr
+              <span className="text-muted-foreground text-lg pl-4 hidden sm:inline">
+                Art is <span className="italic underline">not</span> timeless.
+              </span>
+            </h1>
             <Separator orientation="vertical" />
-            <h2 className="text-muted-foreground">
-              Art is <span className="italic underline">not</span> timeless.
-            </h2>
-            <ModeToggle className="ml-auto" />
+            <div className="flex items-center gap-4 sm:gap-8">
+              <Link href="/">
+                <Button variant="link" className="p-0">
+                  All
+                </Button>
+              </Link>
+              <Link href="/essentials">
+                <Button variant="link" className="p-0">
+                  Popular
+                </Button>
+              </Link>
+            </div>
+            <ModeToggle className="ml-auto hidden sm:inline-flex" />
           </header>
           {children}
         </ThemeProvider>
